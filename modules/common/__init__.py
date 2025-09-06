a11b59755e9fd7853918969b901363c85dd15bee
wang301208 <wang301208@protonmail.com>
2025-09-06 21:48:59 +0800

    Add emotion analysis module and state tracking

--- modules/common/__init__.py
+++ modules/common/__init__.py
@@ -2,6 +2,7 @@
 from .exceptions import AutoGPTException, log_and_format_exception
 from .async_utils import run_async
 from .concepts import ConceptNode, ConceptRelation
+from .emotion import EmotionAnalyzer, EmotionState, adjust_response_style
 
 __all__ = [
     "AutoGPTException",
@@ -9,4 +10,7 @@ __all__ = [
     "run_async",
     "ConceptNode",
     "ConceptRelation",
+    "EmotionAnalyzer",
+    "EmotionState",
+    "adjust_response_style",
 ]
