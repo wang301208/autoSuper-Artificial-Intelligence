e5761e1b7a27be99e1e8aa2be161e188413aa821
wang301208 <wang301208@protonmail.com>
2025-09-06 21:27:33 +0800

    feat: add global workspace and metrics

--- backend/monitoring/__init__.py
+++ backend/monitoring/__init__.py
@@ -6,6 +6,7 @@ from .system_metrics import SystemMetricsCollector
 from .metrics_collector import MetricsCollector
 from .performance_monitor import PerformanceMonitor, email_alert, dashboard_alert
 from .reflection import Reflection
+from .global_workspace import GlobalWorkspace, global_workspace
 
 __all__ = [
     "TimeSeriesStorage",
@@ -16,4 +17,6 @@ __all__ = [
     "email_alert",
     "dashboard_alert",
     "Reflection",
+    "GlobalWorkspace",
+    "global_workspace",
 ]
