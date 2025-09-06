e45a9fb954ceaa8112904bebd893c7c721020a15
wang301208 <wang301208@protonmail.com>
2025-09-06 18:19:02 +0800

    feat: add multitask trainer and module registry

---
+++ backend/capability/__init__.py
@@ -0,0 +1,13 @@
+from .module_registry import (
+    available_modules,
+    combine_modules,
+    get_module,
+    register_module,
+)
+
+__all__ = [
+    "available_modules",
+    "combine_modules",
+    "get_module",
+    "register_module",
+]
