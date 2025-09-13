a0c576dd641c1321c1401b35c6aa146af7d2d8be
wang301208 <wang301208@protonmail.com>
2025-09-13 11:46:04 +0800

    Add tests for algorithms submodules and CI workflow

---
+++ algorithms/tests/test_sorting.py
@@ -0,0 +1,24 @@
+import pytest
+
+from algorithms.sorting.basic.bubble_sort import BubbleSort
+from algorithms.sorting.basic.quick_sort import QuickSort
+
+
+@pytest.mark.parametrize("algorithm", [BubbleSort(), QuickSort()])
+def test_sorting_basic(algorithm):
+    data = [5, 1, 4, 2, 8]
+    original = list(data)
+    assert algorithm.execute(data) == sorted(data)
+    assert data == original
+
+
+@pytest.mark.parametrize("algorithm", [BubbleSort(), QuickSort()])
+def test_sorting_empty_and_single(algorithm):
+    assert algorithm.execute([]) == []
+    assert algorithm.execute([1]) == [1]
+
+
+@pytest.mark.parametrize("algorithm", [BubbleSort(), QuickSort()])
+def test_sorting_type_error(algorithm):
+    with pytest.raises(TypeError):
+        algorithm.execute([1, "a"])
