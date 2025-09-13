a76830dbd78e26208c71563672f90e7b38578fc8
wang301208 <wang301208@protonmail.com>
2025-09-13 17:08:57 +0800

    Add selection sort algorithm and tests

--- algorithms/tests/test_sorting.py
+++ algorithms/tests/test_sorting.py
@@ -2,10 +2,11 @@ import pytest
 
 from algorithms.sorting.basic.bubble_sort import BubbleSort
 from algorithms.sorting.basic.quick_sort import QuickSort
+from algorithms.sorting.basic.selection_sort import SelectionSort
 from algorithms.sorting.advanced.merge_sort import MergeSort
 
 
-@pytest.mark.parametrize("algorithm", [BubbleSort(), QuickSort(), MergeSort()])
+@pytest.mark.parametrize("algorithm", [BubbleSort(), QuickSort(), SelectionSort(), MergeSort()])
 def test_sorting_basic(algorithm):
     data = [5, 1, 4, 2, 8]
     original = list(data)
@@ -13,13 +14,13 @@ def test_sorting_basic(algorithm):
     assert data == original
 
 
-@pytest.mark.parametrize("algorithm", [BubbleSort(), QuickSort(), MergeSort()])
+@pytest.mark.parametrize("algorithm", [BubbleSort(), QuickSort(), SelectionSort(), MergeSort()])
 def test_sorting_empty_and_single(algorithm):
     assert algorithm.execute([]) == []
     assert algorithm.execute([1]) == [1]
 
 
-@pytest.mark.parametrize("algorithm", [BubbleSort(), QuickSort(), MergeSort()])
+@pytest.mark.parametrize("algorithm", [BubbleSort(), QuickSort(), SelectionSort(), MergeSort()])
 def test_sorting_type_error(algorithm):
     with pytest.raises(TypeError):
         algorithm.execute([1, "a"])
