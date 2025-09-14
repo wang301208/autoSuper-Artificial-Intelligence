f2bfd7e7cc8db6b26f96352b92ce803b2b5dd893
wang301208 <wang301208@protonmail.com>
2025-09-14 18:00:50 +0800

    Add ethical reasoning engine with rule-based evaluation

--- modules/brain/__init__.py
+++ modules/brain/__init__.py
@@ -14,6 +14,7 @@ from .message_bus import (
 from .multimodal import MultimodalFusionEngine
 from .adapter import BrainModule
 from .bci import BrainComputerInterface
+from .ethics import EthicalReasoningEngine, EthicalRule
 
 __all__ = [
     "VisualCortex",
@@ -32,4 +33,6 @@ __all__ = [
     "MultimodalFusionEngine",
     "BrainModule",
     "BrainComputerInterface",
+    "EthicalReasoningEngine",
+    "EthicalRule",
 ]
